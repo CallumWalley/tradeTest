04eeb9b2db47530e2ad313876c1e662bf89d4080
cal <callumnmw@gmail.com>
2024-09-28 15:41:57 +1200

    stave 4

--- Map/Domains/Planet.cs
+++ Map/Domains/Planet.cs
@@ -15,5 +15,37 @@ public partial class Planet : Domain
 	[Export]
 	double period;
 
+	[ExportGroup("Physical")]
+	[Export]
+	double mass;
+	[Export]
+	public Color albedo { get; set; } = new();
+	[Export]
+	float equatorialRadius; // in Mm
+
+	public override float ZoomLevel {get{return (float)Math.Log(equatorialRadius, (int)PlayerConfig.config.GetValue("interface", "logBase"));}}
+
+    public override void _Ready()
+    {
+        base._Ready();
+		// // UI Stuff
+        // CircleShape2D circleShape2D = new CircleShape2D();
+        // circleShape2D.Radius = UIRadius;
+        // collisionShape2D = new();
+        // collisionShape2D.Shape = circleShape2D;
+        // // Add interactive 
+        // Area2D area2D = new();
+        // area2D.AddChild(collisionShape2D);
+
+        // area2D.Connect("mouse_entered", new Callable(this, "Focus"));
+        // area2D.Connect("mouse_exited", new Callable(this, "UnFocus"));
+        // AddChild(area2D);
+    }
+
+	public override void _Draw(){
+		base._Draw();
+		DrawCircle(this.Position, ZoomLevel, albedo);
+	}
+    
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 }
